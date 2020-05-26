7ba815a1c0a4b82f3322ccb0de398d439260807b
jaemin <yjm8440@naver.com>
2020-05-27 01:59:23 +0900

    file upload

--- src/main/java/kr/ac/hansung/cse/controller/AdminController.java
+++ src/main/java/kr/ac/hansung/cse/controller/AdminController.java
@@ -1,7 +1,13 @@
 package kr.ac.hansung.cse.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +18,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.ac.hansung.cse.model.Product;
 import kr.ac.hansung.cse.service.ProductService;
@@ -52,7 +59,7 @@ public class AdminController {
 
 	// web form data -> object(filled with form data)
 	@RequestMapping(value = "/productInventory/addProduct", method = RequestMethod.POST)
-	public String addProductPost(@Valid Product product, BindingResult result) {
+	public String addProductPost(@Valid Product product, BindingResult result, HttpServletRequest request) {
 
 		if(result.hasErrors()) {
 			System.out.println("=== Web Form data has some errors ===");
@@ -64,7 +71,33 @@ public class AdminController {
 			return "addProduct";
 		}
 		
-		System.out.println(product);
+		MultipartFile productImage = product.getProductImage();
+		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+		Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + productImage.getOriginalFilename());
+		
+		if(productImage.isEmpty() == false) {
+			System.out.println("------------- file start -------------");
+			System.out.println("name : " + productImage.getName());
+			System.out.println("filename : " + productImage.getOriginalFilename());
+			System.out.println("size : " + productImage.getSize());
+			System.out.println("savePath : " + savePath);
+			System.out.println("------------- file end -------------\n");	
+		}
+		
+		if(productImage != null && !productImage.isEmpty()) {
+			try {
+				productImage.transferTo(new File(savePath.toString()));
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		product.setImageFilename(productImage.getOriginalFilename());
+		
 		productService.addProduct(product);
 
 		return "redirect:/admin/productInventory";
@@ -72,9 +105,21 @@ public class AdminController {
 	}
 
 	@RequestMapping(value = "/productInventory/deleteProduct/{id}", method = RequestMethod.GET)
-	public String deleteProduct(@PathVariable int id) {
+	public String deleteProduct(@PathVariable int id, HttpServletRequest request) {
 
 		Product product = productService.getProductById(id);
+		
+		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+		Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + product.getImageFilename());
+		
+		if(Files.exists(savePath)) {
+			try {
+				Files.delete(savePath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		productService.deleteProduct(product);
 
 		return "redirect:/admin/productInventory";
@@ -92,7 +137,7 @@ public class AdminController {
 	
 	
 	@RequestMapping(value = "/productInventory/updateProduct", method = RequestMethod.POST)
-	public String updateProductPost(@Valid Product product, BindingResult result) {
+	public String updateProductPost(@Valid Product product, BindingResult result, HttpServletRequest request) {
 
 		if(result.hasErrors()) {
 			System.out.println("=== Web Form data has some errors ===");
@@ -103,6 +148,33 @@ public class AdminController {
 			}
 			return "updateProduct";
 		}
+		
+		MultipartFile productImage = product.getProductImage();
+		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+		Path savePath = Paths.get(rootDirectory + "\\resources\\images\\" + productImage.getOriginalFilename());
+		
+		if(productImage.isEmpty() == false) {
+			System.out.println("------------- file start -------------");
+			System.out.println("name : " + productImage.getName());
+			System.out.println("filename : " + productImage.getOriginalFilename());
+			System.out.println("size : " + productImage.getSize());
+			System.out.println("savePath : " + savePath);
+			System.out.println("------------- file end -------------\n");	
+		}
+		
+		if(productImage != null && !productImage.isEmpty()) {
+			try {
+				productImage.transferTo(new File(savePath.toString()));
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		product.setImageFilename(productImage.getOriginalFilename());
 		
 		productService.updateProduct(product);
 
--- src/main/webapp/WEB-INF/views/viewProduct.jsp
+++ src/main/webapp/WEB-INF/views/viewProduct.jsp
@@ -1,6 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="sf"%>
 
@@ -13,7 +11,7 @@
 			<div class="col-md-6">
 				<c:set var="imageFilename"
 					value="/resources/images/${product.id}.jpg" />
-				<img src="<c:url value="${imageFilename}" />" alt="image"
+				<img src="<c:url value="${product.imageFilename}" />" alt="image"
 					style="width: 80%" />
 
 			</div>
@@ -41,3 +39,4 @@
 			</div>
 		</div>
 	</div>
+</div>
